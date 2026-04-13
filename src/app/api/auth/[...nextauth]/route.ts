0a36c2ce9d03566cbf14e75df0a9f20ca639bc10
Michail <astapenko.m99@gmail.com>
2026-04-13 09:14:39 +0200

    fix: fix problem with build

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,7 @@
-import { handlers } from '@/auth';
+import NextAuth from 'next-auth';
 
-export const { GET, POST } = handlers;
+import { authOptions } from '@/auth';
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
