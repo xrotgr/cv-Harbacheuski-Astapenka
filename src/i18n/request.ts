331ad142f507297aa640710a4361ba57dc1a3e3b
xrotgr <stanislav.gorbachevskiy@gmail.com>
2026-04-15 10:27:05 +0300

    fix: fix problem with messages config

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -9,6 +9,7 @@ export default getRequestConfig(async ({ requestLocale }) => {
   const locale = hasLocale(routing.locales, requested) ? requested : routing.defaultLocale;
 
   return {
+    messages: (await import(`../../messages/${locale}.json`)).default,
     locale,
   };
 });
