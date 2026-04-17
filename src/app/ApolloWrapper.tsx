92a527341799227f9a7a707547b67689c50c26f9
Michail <astapenko.m99@gmail.com>
2026-04-17 16:40:05 +0200

    feat: chenge appolo file headers for auth

--- src/app/ApolloWrapper.tsx
+++ src/app/ApolloWrapper.tsx
@@ -1,20 +1,33 @@
 'use client';
 
 import { HttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import {
   ApolloNextAppProvider,
   ApolloClient,
   InMemoryCache,
 } from '@apollo/client-integration-nextjs';
+import { getSession } from 'next-auth/react';
 
 function makeClient() {
   const httpLink = new HttpLink({
     uri: process.env.NEXT_PUBLIC_GRAPHQL_URL,
   });
 
+  const authLink = setContext(async (_, { headers }) => {
+    const session = await getSession();
+    const token = session?.accessToken;
+    return {
+      headers: {
+        ...headers,
+        ...(token ? { Authorization: `Bearer ${token}` } : {}),
+      },
+    };
+  });
+
   return new ApolloClient({
     cache: new InMemoryCache(),
-    link: httpLink,
+    link: authLink.concat(httpLink),
   });
 }
 
